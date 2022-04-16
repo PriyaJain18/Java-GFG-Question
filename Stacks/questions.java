3483baec10859e8f05b1fe9cbae559db506cef5e
PriyaJain18 <preeyajain09@gmail.com>
2022-04-16 23:01:30 +0530

    april16

--- Stacks/questions.java
+++ Stacks/questions.java
@@ -1,5 +1,63 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Stack;
 
+class TwoStacksin1Arr{
+    int top1,top2,cap;
+    int arr[];
+    TwoStacksin1Arr(int n){
+        cap = n;
+        top1 = -1;
+        top2 = cap;
+        arr = new int[n];
+    }
+    void push1(int x){
+        if(top1 == (top2-1)){
+            System.out.println("cannot insert in full stack");
+            return;
+        }
+        top1++;
+        arr[top1] = x;  
+        return;
+    }
+    int peek1(){
+        if(top1 == -1) return Integer.MAX_VALUE;  //stack underflow
+        return arr[top1];
+    }  
+    int pop1(){
+        if(top1 == -1) return Integer.MAX_VALUE;   //stack underflow
+        int res = arr[top1];
+        top1--;
+        return res;
+    }
+
+    void push2(int x){
+        if(top1 == (top2-1)){
+            System.out.println("cannot insert in full stack");
+            return;
+        }
+        top2--;
+        arr[top2] = x;
+        return;
+    }
+    int peek2(){
+        if(top2 == arr.length) return Integer.MAX_VALUE;        //stack underflow //System.exit()
+        return arr[top2];
+    }
+    int pop2(){
+        if(top2 == arr.length) return Integer.MAX_VALUE;        //stack underflow
+        int res = arr[top2];
+        top2++;
+        return res;
+    }
+    void printArr(){
+        for(int i=0;i<cap;i++){
+            System.out.print(arr[i]);
+            // arr1[i]
+        }
+
+    }
+}
 public class questions {
 
     private static boolean balancedBrackets(String st){
@@ -59,8 +117,10 @@ public class questions {
 
         }
 
-        return (stack.isEmpty());
+        return (stack.isEmpty());           
     }
+  
+  
     public static void main(String[] args) {
         //Check if brackets are Balanced : O(N) -> Time and space 
         System.out.println(balancedBrackets("[{}]"));
@@ -68,8 +128,25 @@ public class questions {
 
         System.out.println(balancedBrackets2("[{}]"));
         System.out.println(balancedBrackets2("}{][]})"));
-        
-        
+        TwoStacksin1Arr arr1 = new TwoStacksin1Arr(6);
+        arr1.push1(10);
+        arr1.push1(25);
+        arr1.push2(65);
+        // System.out.println(Arrays.toString(arr1));  //10 25 ----65
+        System.out.println(arr1.pop2());  //65
+        arr1.push2(60);
+        arr1.push2(50);
+        arr1.push2(40);
+        System.out.println(arr1.pop1());  //25
+       
+        // System.out.println(Arrays.toString(arr1));     //10 - - 40 50 60 
+        arr1.push1(20);
+        arr1.push1(30);
+        arr1.printArr();  //10 20 30 40 50 60
+       
+
+
+
     }
     
 }
